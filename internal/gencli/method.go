245ecd5cf7208ad13aae0633229ac9e47b36f6fe
YANG Xudong <yangxudong@ymatrix.cn>
2022-06-22 10:33:49 +0800

    Fix vairable name.

--- internal/gencli/method.go
+++ internal/gencli/method.go
@@ -126,7 +126,7 @@ func (c newMethodCommandCodeGenerator) generateCode(g *protogen.GeneratedFile) e
 			g.P(logError, "(cmd.Context(), err)")
 			g.P(osExit, "(1)")
 			g.P("}")
-			g.P("for response, err := streamClient.Recv();; {")
+			g.P("for response, err := streamingClient.Recv();; {")
 			g.P("if err != nil {")
 			eof := protogen.GoIdent{
 				GoImportPath: "io",
