65749d83ec3199999e71587e20df13efb4939092
Giovanni Beri <giovanni.beri@einride.tech>
2025-11-21 12:01:16 +0100

    feat: support optional flags
    PE-1229

--- aipcli/command_test.go
+++ aipcli/command_test.go
@@ -3,9 +3,71 @@ package aipcli
 import (
 	"testing"
 
+	"github.com/spf13/cobra"
+	"go.einride.tech/aip-cli/aipcli/testservice"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"gotest.tools/v3/assert"
 )
+
+func Test_resetOptionalFlags(t *testing.T) {
+	t.Parallel()
+	cmd := &cobra.Command{}
+	var (
+		pageSize        int32
+		pageToken       string
+		myOptionalField string
+	)
+	cmd.Flags().Int32Var(&pageSize, "page-size", 0, "")
+	cmd.Flags().StringVar(&pageToken, "page-token", "", "")
+	cmd.Flags().StringVar(&myOptionalField, "my-optional-field", "", "")
+
+	// Simulate "my-optional-field" being annotated as a proto value.
+	cmd.Flags().SetAnnotation(
+		"my-optional-field",
+		fieldNameAnnotation,
+		[]string{"einride.example.freight.v1.ListShippersRequest.my_optional_field"},
+	)
+
+	t.Run("optional not provided", func(t *testing.T) {
+		t.Parallel()
+		request := &aipclitestservice.ListShippersRequest{}
+		resetOptionalFlags(cmd, request)
+		assert.Assert(t, request.MyOptionalField == nil)
+	})
+
+	for _, tt := range []struct {
+		name  string
+		value string
+	}{
+		{
+			name:  "empty string",
+			value: "",
+		},
+		{
+			name:  "non-empty string",
+			value: "value",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			request := &aipclitestservice.ListShippersRequest{
+				MyOptionalField: proto.String(tt.value),
+			}
+			assert.NilError(t, cmd.Flags().Set("my-optional-field", tt.value))
+			resetOptionalFlags(cmd, request)
+			assert.Equal(t, "value", *request.MyOptionalField)
+		})
+	}
+	t.Run("optional provided - empty string", func(t *testing.T) {
+		request := &aipclitestservice.ListShippersRequest{
+			MyOptionalField: proto.String("value"),
+		}
+		assert.NilError(t, cmd.Flags().Set("my-optional-field", "new-value"))
+		resetOptionalFlags(cmd, request)
+		assert.Equal(t, "value", *request.MyOptionalField)
+	})
+}
 
 func Test_qualifiedServiceUse(t *testing.T) {
 	for _, tt := range []struct {
