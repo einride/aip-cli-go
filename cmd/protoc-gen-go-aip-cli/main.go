9b2da40a9441c8406874e65857234a5a7cffcfcb
Giovanni Beri <giovanni.beri@einride.tech>
2025-11-21 22:14:51 +0100

    feat: add support for optional
    PE-1229

--- cmd/protoc-gen-go-aip-cli/main.go
+++ cmd/protoc-gen-go-aip-cli/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/pflag"
 	"go.einride.tech/aip-cli/internal/gengoaipcli"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
@@ -11,6 +12,7 @@ func main() {
 	var config gengoaipcli.Config
 	config.AddToFlagSet(flagSet)
 	protogen.Options{ParamFunc: flagSet.Set}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		return gengoaipcli.Run(gen, config)
 	})
 }
