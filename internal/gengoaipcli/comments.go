5c20576557457e2b46d59871b0e44b80305c5f11
Eric Wennerberg <ericwenn@gmail.com>
2025-12-23 13:26:59 +0100

    fix: prevent stack overflow when collecting comments
    Currently the generator will panic due to stack overflow when generating
    from a proto descriptor with cycles. For example
    `google.protobuf.Struct` contains such loops.
    
    This commit fixes this issue by:
    * Collecting comments for top level messages
    * When encountering the same message again, exit early

--- internal/gengoaipcli/comments.go
+++ internal/gengoaipcli/comments.go
@@ -1,6 +1,8 @@
 package gengoaipcli
 
-import "google.golang.org/protobuf/reflect/protoreflect"
+import (
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
 
 func collectDescriptorComments(comments map[protoreflect.FullName]string, desc protoreflect.Descriptor) {
 	comments[desc.FullName()] = desc.ParentFile().SourceLocations().ByDescriptor(desc).LeadingComments
@@ -12,6 +14,11 @@ func collectMethodComments(comments map[protoreflect.FullName]string, method pro
 }
 
 func collectMessageComments(comments map[protoreflect.FullName]string, msg protoreflect.MessageDescriptor) {
+	if _, ok := comments[msg.FullName()]; ok {
+		// already collected comments for this message
+		return
+	}
+	collectDescriptorComments(comments, msg)
 	for i := 0; i < msg.Fields().Len(); i++ {
 		field := msg.Fields().Get(i)
 		collectDescriptorComments(comments, field)
